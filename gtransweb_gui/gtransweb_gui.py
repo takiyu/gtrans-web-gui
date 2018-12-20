40c23164df9984f81ffa2298b95efd7292f10bf9
takiyu <takiyu1025.txu.development@gmail.com>
2018-12-20 23:26:36 +0900

    Implement window

--- gtransweb_gui/gtransweb_gui.py
+++ gtransweb_gui/gtransweb_gui.py
@@ -17,36 +17,59 @@ logger.addHandler(NullHandler())
 
 class GTransWebGui(object):
     def __init__(self):
-        pass
-
-    def run(self):
         # Qt application
-        app = QtWidgets.QApplication([sys.argv[0]])
+        self._app = QtWidgets.QApplication([sys.argv[0]])
 
-        window = Window()
+        # Main window
+        self._window = Window(self._translate)
+        # Clipboard and its handler
+        self._clipboard = Clipboard(self._app)
+        self._clip_handler = ClipboardHandler(self._clipboard)
+        self._clip_handler.set_callback(self._on_clip_changed)
+        # Translation engine
+        self._gtrans_async = GTransWebAsync(headless=True)
+        self._gtrans_async.set_callback(self._on_trans_finished)
+        atexit.register(self._gtrans_async.exit)
+        # Buffer for selection mode
+        self._select_buf = CallableBuffer()
+        self._select_buf.set_buftime(3)
 
         # Qt settings
         qsettings = QtCore.QSettings('gtransweb-gui', 'gtanswebgui')
 
-        def on_clip_changed(src_text):
-            select_buf(gtrans_async.translate, 'en', 'ja', src_text)
+    def run(self):
+        ''' Run main application '''
+        self._app.exec_()
 
-        def on_trans_finished(tgt_text):
-            handler.overwrite_clip(tgt_text)
+    def _translate(self, src_text=None):
+        ''' Translate passed text. If not passed, it will be get from GUI. '''
+        # Get languages from GUI
+        src_lang, tgt_lang = self._window.get_langs()
+        # Source text
+        if src_text is None:
+            # Get text from GUI
+            src_text = self._window.get_src_text()
+        else:
+            # Set text to GUI
+            self._window.set_src_text(src_text)
+        # Start translation
+        self._gtrans_async.translate(src_lang, tgt_lang, src_text)
 
-        clipboard = Clipboard(app)
-        handler = ClipboardHandler(clipboard)
-        handler.set_callback(on_clip_changed)
+    def _on_clip_changed(self, src_text):
+        ''' When clipboard changed, start to translate. '''
+        if self._clipboard.get_mode_str() == 'select':
+            # Postpone by buffering
+            self._select_buf(self._translate, src_text)
+        else:
+            # Translate right now
+            self._translate(src_text)
 
-        select_buf = CallableBuffer()
-        select_buf.set_buftime(3)
-
-        gtrans_async = GTransWebAsync(headless=False)
-        gtrans_async.set_callback(on_trans_finished)
-        atexit.register(gtrans_async.exit)
-
-        # Start
-        app.exec_()
+    def _on_trans_finished(self, tgt_text):
+        ''' When translation finished, set to GUI and clipboard. '''
+        # Set to GUI
+#         self._window.set_tgt_text(tgt_text)
+        # Set to clipboard
+        self._clip_handler.overwrite_clip(tgt_text)
 
 
 if __name__ == '__main__':
--- gtransweb_gui/window.py
+++ gtransweb_gui/window.py
@@ -6,76 +6,165 @@ from logging import getLogger, NullHandler
 logger = getLogger(__name__)
 logger.addHandler(NullHandler())
 
+LANGUAGES = {
+    'Auto':      'auto',  'Arabic':    'ar', 'Chinese': 'zh-CN',
+    'English':   'en',    'Esperanto': 'eo', 'French':  'fr',
+    'German':    'de',    'Greek':     'el', 'Italian': 'it',
+    'Japanese':  'ja',    'Korean':    'ko', 'Latin':   'la',
+    'Portugese': 'pt-PT', 'Russian':   'ru', 'Spanish': 'es',
+}
+LANGUAGES_INV = {v: k for k, v in LANGUAGES.items()}
+
 
 class Window(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, trans_func):
         logger.debug('New window is created')
         super(Window, self).__init__()
+        self._trans_func = trans_func
 
         # Set window types
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.Dialog)
         self.setWindowTitle('GtransWeb')
 
+        # Create GUI parts and layout
+        self._gui_parts = GuiParts(parent=self)
+        self._gui_layout = GuiLayout(self._gui_parts)
+        # Connect event functions
+        self._gui_parts.set_connections(self._trans_func, self.swap_langs)
+        # Set initial languages
+        self.set_langs('auto', 'ja')
+
+        # GUI configuration
         self._qsettings = QtCore.QSettings('gtransweb-gui', 'window')
+        # Load window geometry and state
+        self._load_geometry(self._qsettings)
+        self._gui_layout.load_splitter_state(self._qsettings)
 
-        self._init_gui()
+        # Set layout
+        self._gui_layout.set_root(self)
 
+        # Show window
         self.show()
         self.raise_()
 
-    def closeEvent(self, event):
-        # Save window geometry and state
-        self._qsettings.setValue("geometry", self.saveGeometry())
-        self._qsettings.setValue("splitter_state", self.splitter.saveState())
+    def get_langs(self):
+        ''' Get source and target languages '''
+        src = self._gui_parts.src_lang_box.currentText()
+        tgt = self._gui_parts.tgt_lang_box.currentText()
+        return LANGUAGES[src], LANGUAGES[tgt]
 
-    def _init_gui(self):
-        # Create a target text box
-        self.tgt_box = QtWidgets.QTextEdit(self)
+    def set_langs(self, src, tgt):
+        ''' Set source and target languages '''
+        # Escape `auto` target
+        if tgt == 'auto':
+            if src == 'ja':
+                tgt = 'en'
+            else:
+                tgt = 'ja'
+        # Find indices by text
+        src_idx = self._gui_parts.src_lang_box.findText(LANGUAGES_INV[src])
+        tgt_idx = self._gui_parts.tgt_lang_box.findText(LANGUAGES_INV[tgt])
+        # Set indices
+        self._gui_parts.src_lang_box.setCurrentIndex(src_idx)
+        self._gui_parts.tgt_lang_box.setCurrentIndex(tgt_idx)
+
+    def get_src_text(self):
+        ''' Get text from source text box '''
+        return self._gui_parts.src_box.toPlainText()
+
+    def set_src_text(self, text):
+        ''' Set text to source text box '''
+        self._gui_parts.src_box.setHtml(text)
+
+    def set_tgt_text(self, text):
+        ''' Set text to target text box '''
+        self._gui_parts.tgt_box.setHtml(text)
+
+    def swap_langs(self):
+        ''' Swap source and target languages '''
+        src, tgt = self.get_langs()
+        self.set_langs(tgt, src)
+
+    def closeEvent(self, event):
+        ''' Overridden method to save window states at exit '''
+        self._save_geometry(self._qsettings)
+        self._gui_layout.save_splitter_state(self._qsettings)
+
+    def _load_geometry(self, qsettings):
+        geom = qsettings.value('geometry')
+        if geom is not None:
+            self.restoreGeometry(geom)
+
+    def _save_geometry(self, qsettings):
+        qsettings.setValue('geometry', self.saveGeometry())
+
+
+class GuiParts(object):
+    def __init__(self, parent):
+        # Create Gui widget parts
+        self.tgt_box = QtWidgets.QTextEdit(parent)
+        self.src_box = QtWidgets.QTextEdit(parent)
+        self.swap_btn = QtWidgets.QPushButton("<-->", parent)
+        self.src_lang_box = QtWidgets.QComboBox(parent)
+        self.tgt_lang_box = QtWidgets.QComboBox(parent)
+        self.trans_btn = QtWidgets.QPushButton("Translate", parent)
+
+        # Set part styles
+        self._set_styles()
+
+    def set_connections(self, trans_func, swap_langs):
+        # Connect functions
+        self.trans_btn.clicked.connect(lambda: trans_func())
+        self.swap_btn.clicked.connect(lambda: swap_langs())
+
+    def _set_styles(self):
+        # Set GUI styles
         self.tgt_box.setReadOnly(True)
         self.tgt_box.setAcceptRichText(True)
-        # Create a source text box
-        self.src_box = QtWidgets.QTextEdit(self)
         self.src_box.setAcceptRichText(True)
-        # Create bottom items
-        self.src_lang_box = QtWidgets.QLineEdit(self)
-        self.tgt_lang_box = QtWidgets.QLineEdit(self)
-        self.src_lang_box.setFixedWidth(50)
-        self.tgt_lang_box.setFixedWidth(50)
-        # show candidate list when clicked
-#         self.src_lang_box.focusInEvent = lambda _: self._show_candidates(1)
-#         self.tgt_lang_box.focusInEvent = lambda _: self._show_candidates(2)
-        self.swap_btn = QtWidgets.QPushButton("<-->", self)
         self.swap_btn.setFixedWidth(50)
-#         self.swap_btn.clicked.connect(self._swap_langs)
-        self.trans_btn = QtWidgets.QPushButton("Translate", self)
-        self.trans_btn.clicked.connect(lambda: self.translate())
+        self.src_lang_box.setFixedWidth(90)
+        self.tgt_lang_box.setFixedWidth(90)
+        self.src_lang_box.addItems(LANGUAGES.keys())
+        self.tgt_lang_box.addItems(LANGUAGES.keys())
 
+
+class GuiLayout(object):
+    def __init__(self, gui_parts):
         # Create a splitter for text box
-        self.splitter = QtWidgets.QSplitter(QtCore.Qt.Vertical)
-        self.splitter.addWidget(self.tgt_box)
-        self.splitter.addWidget(self.src_box)
-        self.splitter.setCollapsible(0, False)
-        self.splitter.setCollapsible(1, False)
+        self._splitter = QtWidgets.QSplitter(QtCore.Qt.Vertical)
+        self._splitter.addWidget(gui_parts.tgt_box)
+        self._splitter.addWidget(gui_parts.src_box)
+        self._splitter.setCollapsible(0, False)
+        self._splitter.setCollapsible(1, False)
 
         # Create horizontal bottom layout
-        self.bottom_layout = QtWidgets.QHBoxLayout()
-        self.bottom_layout.addWidget(self.src_lang_box)
-        self.bottom_layout.addWidget(self.swap_btn)
-        self.bottom_layout.addWidget(self.tgt_lang_box)
-        self.bottom_layout.addWidget(self.trans_btn)
+        self._bottom_layout = QtWidgets.QHBoxLayout()
+        self._bottom_layout.addWidget(gui_parts.src_lang_box)
+        self._bottom_layout.addWidget(gui_parts.swap_btn)
+        self._bottom_layout.addWidget(gui_parts.tgt_lang_box)
+        self._bottom_layout.addWidget(gui_parts.trans_btn)
         # Warp with a widget
-        self.bottom_widget = QtWidgets.QWidget()
-        self.bottom_widget.setLayout(self.bottom_layout)
-        self.bottom_widget.setContentsMargins(-5, -5, -5, -5)
+        self._bottom_widget = QtWidgets.QWidget()
+        self._bottom_widget.setLayout(self._bottom_layout)
+        self._bottom_widget.setContentsMargins(-5, -5, -5, -5)
 
         # Create vertical central layout
-        self.central_layout = QtWidgets.QVBoxLayout()
-        self.central_layout.addWidget(self.splitter)
-        self.central_layout.addWidget(self.bottom_widget)
+        self._central_layout = QtWidgets.QVBoxLayout()
+        self._central_layout.addWidget(self._splitter)
+        self._central_layout.addWidget(self._bottom_widget)
         # Warp with a widget
-        self.central_widget = QtWidgets.QWidget()
-        self.central_widget.setLayout(self.central_layout)
-        self.central_widget.setContentsMargins(-5, -5, -5, -5)
+        self._central_widget = QtWidgets.QWidget()
+        self._central_widget.setLayout(self._central_layout)
+        self._central_widget.setContentsMargins(-5, -5, -5, -5)
 
-        # Set layout
-        self.setCentralWidget(self.central_widget)
+    def set_root(self, root_widget):
+        root_widget.setCentralWidget(self._central_widget)
+
+    def load_splitter_state(self, qsettings):
+        state = qsettings.value('splitter_state')
+        if state is not None:
+            self._splitter.restoreState(state)
+
+    def save_splitter_state(self, qsettings):
+        qsettings.setValue('splitter_state', self._splitter.saveState())
